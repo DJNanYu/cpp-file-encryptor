3dbb37f8c763d5dc04a6be3a6de84f406ce3b30d
DJNanYu <2986945687@qq.com>
2024-02-09 10:00:00 -0500

    feat: initial draft of cpp-file-encryptor

---
+++ README.md
@@ -0,0 +1,5 @@
+# cpp-file-encryptor
+
+JNI Layer Security Implementation. JNI层安全实现
+
+Initial development of cpp-file-encryptor.
---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Draft version for cpp-file-encryptor
+
+
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+#define LOG_TAG "NativeSecurity"
+#defi...
